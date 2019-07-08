d1b4c5563ceedf7554b30b6224defbee47bad79e
octavio <serrayon81@gmail.com>
2019-07-08 09:35:00 -0700

    game rules

--- js/app.js
+++ js/app.js
@@ -5,22 +5,23 @@ class Cards {
     }
 };
 
-class Deck {
-constructor(){
-this.suit = ["spades", "diamonds", "clubs", "hearts"];
-this.value = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
-this.cards = this.createDeck();
 
-}
-createDeck() {
-    let cards = []
-    for (let i = 0; i < this.suit.length; i++){
-        for (let j = 0; j < this.value.length; j++){
-         cards.push({suit: this.suit[i] , value: this.value[j]})
-        }
+class Deck {
+    constructor(){
+        this.suit = ["spades", "diamonds", "clubs", "hearts"];
+        this.value = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+        this.cards = this.createDeck();
     }
-    return cards
-}
+
+createDeck() {
+        let cards = []
+        for (let i = 0; i < this.suit.length; i++){
+            for (let j = 0; j < this.value.length; j++){
+            cards.push({suit: this.suit[i] , value: this.value[j]})
+            }
+        }
+        return cards
+    }
 shuffle() {
     // fisher yates shuffle https://bost.ocks.org/mike/shuffle/
     var m = this.cards.length, t, i;
@@ -41,63 +42,111 @@ shuffle() {
     return this.cards.pop();
  }
 }; 
-deck = new Deck();
+
 
 
 class Player {
     constructor() {
         this.hand = [];
+        
     
     }
-    // handValue(){
-    //    let total =0;
-    //     this.hand.forEach (card => {
-    //         if (card.value === "J"){
-    //             total =+10
-    //         } else if (card.value === "Q"){
-    //             total =+10
-    //         } else if (card.value === "K"){
-    //             total =+10
-    //         } 
-    //         else{
-    //             total =+ card.value
-    //         }
-    //     })
-    //     return total;
-    //}
-    //gameRules(){
+    handValue(){
+       let total =0;
+        this.hand.forEach (card => {
+            if (card.value === "J"){
+                total +=10
+            } else if (card.value === "Q"){
+                total +=10
+            } else if (card.value === "K"){
+                total +=10
+            } 
+            else if(card.value === "A"){
+                total += 11;
+            }
+            else{
+                total += card.value
+            }
+            console.log("Total Value in loop : "+total);
+        });
+        console.log("Length of Card : "+total,this.hand.length);
+        return total;
+    }
 
-    //}
+    pushCard(obj){
+        this.hand.push(obj.deal());
+    }
 };
 
-player1 = new Player();
-dealer = new Player();
-
-console.log(player1.hand = player1.hand.concat(deck.deal()));
-dealer.hand = dealer.hand.concat(deck.deal());
 
 
-// function gameRules() {
-//     if(player1 > 21) {
-//         console.log(`Player one BUSTED you lose!`)
-//     }   else if(player1 === 21) {
-//         console.log(`Player one WON!`);
-//     }   else if(player1 < 21 && player1 > dealer) {
-//         console.log(`Player one WON!`); 
-//     }   else if (dealer > 21) {
-//         console.log(`Dealer BUSTED!`) 
-//     }   else if(dealer < 21 && dealer > player1) {
-//         console.log(`Dealer WON!`);
-//     }   else if(dealer === 17) {
-//         (dealer stands) 
-//     }   else if (dealer < 16) {
-//             (dealer hits)
-//  };
+
+function gameRules() {
+    console.log("In game rule");
+    if(player1.handValue() > 21) {
+        console.log(`Player one BUSTED you lose!`)
+        document.getElementById('message').innerHTML ="Player one BUSTED you lose!"
+    }   else if(player1.handValue() === 21) {
+        document.getElementById('message').innerHTML ="Player one WON!"
+        console.log(`Player one WON!`);
+    }   else if(player1.handValue() < 21 && player1.handValue() > dealer.handValue()) {
+        document.getElementById('message').innerHTML ="Player one WON!"
+        console.log(`Player one WON!`); 
+    }   else if (dealer.handValue() > 21) {
+        document.getElementById('message').innerHTML ="Dealer BUSTED!"
+        console.log(`Dealer BUSTED!`) 
+    }   else if(dealer.handValue() < 21 && dealer.handValue() > player1.handValue()) {
+        document.getElementById('message').innerHTML ="Dealer WON!"
+        console.log(`Dealer WON!`);
+    }
+    else if(dealer.handValue()=== player1.handValue()){
+        document.getElementById('message').innerHTML ="Game tide"
+        console.log("Game tide");
+    }
+    // }   else if(dealer.handValue() === 17) {
+    //     //(dealerstands) 
+    // }   else if (dealer.handValue() < 16){}
+           // (dealer hits)
+ }
 
 
 //------------event listeners--------//
 
 // for start game on the first page
+// deck = new Deck();
+// player1 = new Player();
+// dealer = new Player();
+// start();
+// function start()
+// {
+    
+        
+//     deck.shuffle();
+//     player1.pushCard(deck);
+//     player1.pushCard(deck);
+//     dealer.pushCard(deck);
+
+//     this.print();
+
+// }
+
+// f
 // one for hit
-//one for stand
-//one for shuffle
+// function hit()
+// {
+//     player1.pushCard(deck);
+//     this.gameRules(); 
+//     this.print();
+// }
+// //one for stand
+// function stand()
+// {
+//     console.log("In stand ",dealer.handValue());
+//     while(dealer.handValue()<17)
+//     {
+//         dealer.pushCard(deck);
+//     }
+//     this.print();
+//     this.gameRules();
+// }
+// //one for shuffle
